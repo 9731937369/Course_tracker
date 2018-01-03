09b0ebc852b0073392994d57e55d387b18b2ecea
Shivakumara m <shivasorab@gmail.com>
2018-01-03 09:30:16 +0530

    coursetracker first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  rescue_from CanCan::AccessDenied do
+  		redirect_to root_path, notice: "You are not authorized to access this page."
+  end
+ rescue_from SocketError  do
+  	redirect_to root_path, notice: "Make sure You are not connected to internet"
+  end
+end
